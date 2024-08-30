8a10bda3b80f6c03f8b7040ceb25cf3f82e0d189
bob frankel <bob@biosbob.biz>
2024-08-30 06:15:38 -0700

    extension: update token colors

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import { setupZig } from "./zigSetup";
 const ZIG_MODE: vscode.DocumentFilter = { language: "zig", scheme: "file" };
 
 export async function activate(context: vscode.ExtensionContext) {
+    await updateSettings('editor', 'tokenColorCustomizations', COLORS);
     await setupZig(context).finally(() => {
         const compiler = new ZigCompilerProvider();
         compiler.activate(context.subscriptions);
@@ -28,3 +29,53 @@ export async function activate(context: vscode.ExtensionContext) {
 export async function deactivate() {
     await deactivateZls();
 }
+
+function rootPath(): string {
+    return vscode.workspace.workspaceFolders![0].uri.fsPath
+}
+
+async function updateSettings(sect: string, key: string, val: any) {
+    let conf = vscode.workspace.getConfiguration(sect, vscode.Uri.file(rootPath()))
+    await conf.update(key, val, vscode.ConfigurationTarget.Workspace)
+}
+
+const COLORS = 
+    {
+        "textMateRules": [
+            {
+                "scope": "zigem-0",
+                "settings": {
+                    "foreground": "#fa6424",
+                    "fontStyle": "bold"
+                }
+            },
+            {
+                "scope": "zigem",
+                "settings": {
+                    "foreground": "#f99165",
+                    "fontStyle": "bold"
+                }
+            },
+            {
+                "scope": "zigem-2",
+                "settings": {
+                    "foreground": "#c0e3f9",
+                    "fontStyle": "bold"
+                }
+            },
+            {
+                "scope": "zigemDebug",
+                "settings": {
+                    "foreground": "#ff0000",
+                    "fontStyle": "bold"
+                }
+            },
+            {
+                "scope": "zigemMarker",
+                "settings": {
+                    "foreground": "#bbffee",
+                    "fontStyle": "bold"
+                }
+            },
+        ]
+    }
