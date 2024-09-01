1aa98fe7317f7e261bc2f331250041748fd2a83c
bob frankel <bob@biosbob.biz>
2024-09-01 09:15:17 -0700

    VERS stage

--- releng/stage.sh
+++ releng/stage.sh
@@ -5,12 +5,12 @@ DATE=`date -u +%Y%m%d%H%M`
 VERS_FULL="${VERS}.${DATE}"
 TAG="v${VERS_FULL}"
 
-#rm -f vscode-zigem*.vsix
-#rm -f vscode-zigem.zip
+rm -f vscode-zigem*.vsix
+rm -f vscode-zigem.zip
 
-#npx vsce package
-#mv vscode-zigem-${VERS}.vsix vscode-zigem-${VERS_FULL}.vsix
-#zip vscode-zigem.zip vscode-zigem-${VERS_FULL}.vsix
+npx vsce package
+mv vscode-zigem-${VERS}.vsix vscode-zigem-${VERS_FULL}.vsix
+zip vscode-zigem.zip vscode-zigem-${VERS_FULL}.vsix
 
 gh repo set-default https://github.com/em-foundation/vscode-zigem.git
 
