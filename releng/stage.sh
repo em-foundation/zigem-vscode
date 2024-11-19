6a601d99b47ee50d141295bf1a5fc1e5d605f2d5
bob frankel <bob@biosbob.biz>
2024-11-18 16:58:13 -0800

    VERS 0.1.1

--- releng/stage.sh
+++ releng/stage.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-VERS='0.0.3'
+VERS='0.1.1'
 DATE=`date -u +%Y%m%d%H%M`
 VERS_FULL="${VERS}.${DATE}"
 TAG="v${VERS_FULL}"
@@ -8,7 +8,7 @@ TAG="v${VERS_FULL}"
 rm -f vscode-zigem*.vsix
 rm -f vscode-zigem.zip
 
-npx vsce package
+npx vsce package --pre-release
 mv vscode-zigem-${VERS}.vsix vscode-zigem-${VERS_FULL}.vsix
 zip vscode-zigem.zip vscode-zigem-${VERS_FULL}.vsix
 
